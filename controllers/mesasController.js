b1ae2b949ace7aa02c6740c8401c7bfe4caebd1a
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2022-09-08 08:48:49 -0300

    listar mesas

--- controllers/mesasController.js
+++ controllers/mesasController.js
@@ -5,10 +5,6 @@ module.exports = {
     async listarMesas(request, response) { 
         try {
             const sql = 'SELECT mes_id, mes_nome, mes_status, mes_lugares, ped_id FROM mesas;';  
-            //console.log('tam: ' + mesas[0].length);
-            //return response.status(200).json(mesas[0]);   
-            // return response.status(200).json(mesas[0][0]); // apenas 1º item do resultado sql
-            // return response.status(200).json(mesas[0][0].mes_lugares); // apenas o valor de um item retornado
             const mesas = await db.query(sql); 
             return response.status(200).json({confirma: 'Sucesso', nResults: mesas[0].length, message: mesas[0]});  
         } catch (error) { 
