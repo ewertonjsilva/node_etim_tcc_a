8f8b232755ee63f58258b3af8411025b78402eeb
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2022-09-19 11:37:42 -0300

    create mesas

--- controllers/mesasController.js
+++ controllers/mesasController.js
@@ -11,5 +11,23 @@ module.exports = {
             return response.status(500).json({confirma: 'Erro', message: error});
         }
     }, 
+    async create(request, response) {
+        try {
+                // parâmtros passados via corpo da requisição
+            const { mes_nome, mes_status, mes_lugares, ped_id } = request.body;  
+                // instrução sql para inserção
+            const sql = 'INSERT INTO mesas (mes_nome, mes_status, mes_lugares, ped_id) VALUES (?, ?, ?, ?)'; 
+                // definição de array com os parâmetros que receberam os valores do front-end
+            const values = [mes_nome, mes_status, mes_lugares, ped_id]; 
+                // executa a instrução de inserção no banco de dados       
+            const confirmacao = await db.query(sql, values);
+                // Exibe o id do registro inserido
+            const mes_id = confirmacao[0].insertId; 
+                // Mensagem de retorno no formato JSON
+            return response.status(200).json({confirma: 'Sucesso', message: mes_id});
+        } catch (error) { 
+            return response.status(500).json({confirma: 'Erro', message: error});
+        }   
+    }, 
 };
 
