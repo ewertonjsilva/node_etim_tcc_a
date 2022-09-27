df7183f5a255ee867c925a8f634e406def516aed
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2022-09-27 11:31:20 -0300

    delete mesas

--- controllers/mesasController.js
+++ controllers/mesasController.js
@@ -47,5 +47,22 @@ module.exports = {
             return response.status(500).json({confirma: 'Erro', message: error});
         }        
     }, 
+    async delete(request, response) { 
+        try {
+                // parâmetro passado via url na chamada da api pelo front-end
+            const { mes_id } = request.params;
+    
+                // comando de exclusão
+            const sql = 'DELETE FROM mesas WHERE mes_id = ?'; 
+                // definição de array com os parâmetros que receberam os valores do front-end
+            const values = [mes_id];
+                // executa a instrução de exclusão no banco de dados    
+            await db.query(sql, values);  
+                // Mensagem de retorno no formato JSON
+            return response.status(200).json({confirma: 'Sucesso', message:'Mesa com id ' + mes_id + ' excluída com sucesso'}); 
+        } catch (error) {
+            return response.status(500).json({confirma: 'Erro', message: error});
+        }        
+    }, 
 };
 
